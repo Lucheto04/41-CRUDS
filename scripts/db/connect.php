6aa70c3afe9a740eca114390317820989f2e1841
Lucheto04 <lucho.lgrg04@gmail.com>
2023-06-23 21:41:16 -0500

    feat: :sparkles: Hacemos un GET
    Hacemos el GET de la primer tabla para confirmar que si me este funcionando la configuración basica y asi empezar los CRUDS

--- scripts/db/connect.php
+++ scripts/db/connect.php
@@ -1,6 +1,17 @@
 <?php
     namespace App;
-    
+    class connect extends credentials{
+        protected $con;
+        function __construct(private $dns="mysql", private $port = 3306) {
+            try {
+                $this->con = new \PDO($this->dns.":host=".$this->__get('host').";dbname=".$this->__get('dbname').";user=".$this->username.";password=".$this->__get('password').";port=".$this->port);
+                $this->con->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                // print_r("Iniciando...");
 
 
+            } catch (\PDOException $e) {
+                print_r($e->getMessage());
+            }
+        }
+    }
 ?>
--- uploads/app.php
+++ uploads/app.php
@@ -1,7 +1,13 @@
 <?php
     require "../vendor/autoload.php";
     $router = new \Bramus\Router\Router();
-
-
+    // GET
+    $router->get("/camper", function(){
+        $cox = new \App\connect();
+        $res = $cox->con->prepare("SELECT * FROM academic_area");
+        $res->execute();    
+        $res = $res->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($res);
+    });
     $router->run();
 ?>
