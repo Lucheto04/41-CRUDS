5080e31d9e8b8411e5668e0de055800f9ef3ff07
Lucheto04 <lucho.lgrg04@gmail.com>
2023-06-23 21:16:42 -0500

    feat: :sparkles: Creamos la configuraciòn basica
    Ya tenemos la configuración basica para empezar los 41 CRUDS

---
+++ scripts/system.php
@@ -0,0 +1,10 @@
+<?php
+    namespace App;
+    trait system {
+        public function __get($name) {
+            return $this->{$name};
+        }
+    }
+
+
+?>
---
+++ uploads/app.php
@@ -0,0 +1,7 @@
+<?php
+    require "../vendor/autoload.php";
+    $router = new \Bramus\Router\Router();
+
+
+    $router->run();
+?>
