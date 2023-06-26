a3a1840be332e042f581914470c64fa5479a43ab
Lucheto04 <lucho.lgrg04@gmail.com>
2023-06-26 17:13:14 -0500

    feat: :sparkles: Añadimos el front
    Le agregamos todo el front para empezar a consumir las apis

--- uploads/app.php
+++ uploads/app.php
@@ -1,8 +1,4 @@
 <?php
-//! Pequeña funciónes para detectar errores y saber en donde están.
-error_reporting(E_ALL);
-ini_set('display_error', "1");
-
 
 require_once "../vendor/autoload.php";
 $router = new \Bramus\Router\Router();
